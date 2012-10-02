204729507e4f3202c6c10cb8694d2d245ad4da89
Lukas Berg <ljberg@gmx.de>
2012-10-02 12:15:52 +0200

    Loader for modules

---
+++ modules/Modul.php
@@ -0,0 +1,52 @@
+<?php
+
+abstract class Modul {
+
+	private $template;
+
+	public final function display() {
+		include($this->template);
+	}
+
+	public abstract function execute();
+
+	/**
+	 *
+	 * @param string $name
+	 * @throws InvalidArgumentException
+	 * @return Modul
+	 */
+	public static function &loadModul($name, $root = ROOT) {
+		if (!is_string($name)) {
+			throw new InvalidArgumentException($name ." is not a string");
+		}
+
+		$class = ucfirst($name);
+
+		if (!class_exists($class)) {
+			$file = $root.S."modules".S.$name.S.$class.".php";
+
+			if (!file_exists($file)) {
+				throw new InvalidArgumentException("file '". $name ."' does not exist");
+			}
+			
+			include($file);
+			
+			if (!class_exists($class)) {
+				throw new InvalidArgumentException("class '". $class ."' does not exist");
+			}
+		}
+		
+		$modul = new $class();
+		
+		if (!($modul instanceof Modul)) {
+			throw new InvalidArgumentException("'". $class ."' is not an instance of 'Modul'");
+		}
+		
+		$modul->template = $root.S."modules".S.$name.S."template".S.$name.".php";
+
+		return $modul;
+	}
+}
+
+?>
