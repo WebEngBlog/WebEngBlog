deeee761edbdb6edf824c84a7f8d88f2482a1f29
Lukas Berg <ljberg@gmx.de>
2012-10-05 12:04:23 +0200

    Display function to class "System" added

--- system/System.php
+++ system/System.php
@@ -25,6 +25,30 @@ class System {
 	public static function getBrowser() {
 		return $_SERVER['HTTP_USER_AGENT'];
 	}
+	
+	public static function display($dir, $default) {
+		if (!isset($dir) || !is_string($dir) || !is_dir($dir)) {
+			throw new InvalidArgumentException($dir ." is invalid");
+		}
+		
+	//	$display = $_POST["display"];
+		$display = $_GET["display"];
+		
+		if (!isset($display)) {
+			Modul::loadModul($default, $dir)->display();
+			return;
+		}
+		
+		$moduls = explode(';', $display);
+		
+		foreach ($moduls as $modul) {
+			try {
+				Modul::loadModul($modul, $dir)->display();
+			} catch (Exception $e) {
+				echo $modul ." not found";
+			}
+		}
+	}
 }
 
 ?>
