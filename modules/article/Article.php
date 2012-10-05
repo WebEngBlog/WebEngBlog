0881ccb316e079db183bab1b217797831bdd28de
Lukas Berg <ljberg@gmx.de>
2012-10-05 12:02:52 +0200

    New Modul "article" added

---
+++ modules/article/Article.php
@@ -0,0 +1,19 @@
+<?php
+
+class Article extends Modul {
+	
+	public function execute() {
+		
+	}
+	
+	public function &getArticle($id) {
+		if (!is_int($id)) {
+			throw new InvalidArgumentException($id ." is not an int");
+		}
+		
+		
+		return R::load("article", $id);
+	}
+}
+
+?>
---
+++ modules/article/template/article.php
@@ -0,0 +1,15 @@
+<?php
+
+//$article = Modul::loadModul("article")->getArticle((int) $_POST["id"]);
+$article = Modul::loadModul("article")->getArticle((int) $_GET["id"]);
+
+?><article class="article_whole"><?php
+if ($article->id > 0) {
+	?><h1><?php echo $article->title; ?></h1>
+		<div><?php echo $article->content; ?></div><?php
+} else {
+	?><h1>No article found</h1><?php
+}
+?></article><?php
+
+?>
