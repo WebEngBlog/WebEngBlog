573654861dc4c4e887da07d5b11501b71bdc1aaa
Tobias Röding <tobias.roeding@googlemail.com>
2012-10-15 15:49:23 +0200

    ..

---
+++ admin/modules/comments/Comments.php
@@ -0,0 +1,17 @@
+<?php
+/*******************************************************************************
+* Comments modul for the backend (admin area)
+* 
+* @author 		
+* @copyright	
+* @version		
+*******************************************************************************/
+
+class Comments extends Modul {
+
+	public function execute() {
+	
+	}
+}
+
+?>
