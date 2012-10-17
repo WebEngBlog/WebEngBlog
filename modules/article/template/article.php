e213f1dfe72ed408dda3e998e1f631b59c6b670a
Lukas Berg <ljberg@gmx.de>
2012-10-17 11:30:04 +0200

    Modified

--- modules/article/template/article.php
+++ modules/article/template/article.php
@@ -2,7 +2,7 @@
 /*******************************************************************************
 * article template for the frontend
 * 
-* @author 		Lukas Berg, Tobias Röding
+* @author 		Lukas Berg, Tobias R��ding
 * @copyright	@author, 14.10.2012
 * @version		0.9
 *******************************************************************************/
@@ -36,7 +36,7 @@ if ($article->id > 0) {
 <script type="text/javascript">
 
 	function showArticle(id) {
-		loadContent("display=article;comment&id=" + id);
+		loadContent("display=article&id=" + id);
 	}
 	
 </script>
