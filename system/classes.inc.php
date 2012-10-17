55aaa8e68f9c4c241f923e9d429a8f05932c3131
Lukas Berg <ljberg@gmx.de>
2012-10-17 13:55:07 +0200

    no message

--- system/classes.inc.php
+++ system/classes.inc.php
@@ -16,6 +16,6 @@ $classes = array(
 );
 
 include(ROOT.S."system".S."framework".S."rb.php");
-R::setup("mysql:host=localhost;dbname=blog", "root", "root");
+R::setup("mysql:host=localhost;dbname=blog", "root", "");
 
 ?>
