caeb03d7ca9880444c51249450e595e1b5e8e4db
Lukas Berg <ljberg@gmx.de>
2012-10-17 11:30:19 +0200

    Display method modified

--- modules/Modul.php
+++ modules/Modul.php
@@ -11,17 +11,19 @@
 class Modul {
 
 	private static $modules = array();
+	
+	private $root = null;
 
 	public function display($template = null) {
 		if (is_string($template)) {
-			$file = dirname(__FILE__).S.$template.S."template".S.$template.".php";
+			$file = $this->root.S."modules".S.$template.S."template".S.$template.".php";
 		} else {
 			$dir = lcfirst(get_class($this));
-			$file = dirname(__FILE__).S.$dir.S."template".S.lcfirst(get_class($this)).".php";
+			$file = $this->root.S."modules".S.$dir.S."template".S.lcfirst(get_class($this)).".php";
 		}
 		
 		if (!is_file($file)) {
-			throw new InvalidArgumentException($template . " is not a valid file");
+			throw new InvalidArgumentException($file . " is not a valid file");
 		}
 		
 		include($file);
@@ -39,6 +41,10 @@ class Modul {
 	public static final function &loadModul($name, $root = ROOT) {
 		if (!is_string($name) || preg_match("/[^a-z]+/", $name) > 0) {
 			throw new InvalidArgumentException($name ." is invalid");
+		}
+		
+		if (!is_dir($root)) {
+			throw new InvalidArgumentException($root . " is not a valid dir");
 		}
 
 		if (array_key_exists($name, self::$modules)) {
@@ -62,12 +68,11 @@ class Modul {
 		}
 
 		$modul = new $class();
+		$modul->root = $root;
 
 		if (!($modul instanceof Modul)) {
 			throw new InvalidArgumentException("'". $class ."' is not an instance of 'Modul'");
 		}
-
-		//$modul->template = $root.S."modules".S.$name.S."template".S.$name.".php";
 
 		self::$modules[$class] = $modul;
 
