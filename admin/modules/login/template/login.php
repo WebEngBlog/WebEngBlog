45ad4db4cdfbb3ef4e0f4b329261321872e2e077
Lukas Berg <ljberg@gmx.de>
2012-10-19 00:10:30 +0200

    Bug fixed

--- admin/modules/login/template/login.php
+++ admin/modules/login/template/login.php
@@ -1,6 +1,6 @@
 <article id="login">
 	<h3>Login</h3>
-	<form action="" method="post">
+	<form action="index.php" method="post">
 		<fieldset class="login_border">
 			<label id="lbl_username" class="login_label" for="username">Benutzername</label>
 			<input id="in_username" class="input_field" type="text" name="username" />
