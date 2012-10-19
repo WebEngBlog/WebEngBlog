04d4f1ee2270d58d18f6ed3ec9e3e8be5e2d16db
Lukas Berg <ljberg@gmx.de>
2012-10-19 17:47:07 +0200

    Articles modified and search added

--- modules/article/Article.php
+++ modules/article/Article.php
@@ -9,34 +9,38 @@
 
 class Article extends Modul {
 	
-	public function execute() {
-		
-	}
-	
 	public function getArticle($id) {
 		if (!is_int($id)) {
 			throw new InvalidArgumentException($id ." is not an int");
 		}
-
 		return R::load("article", $id);
 	}
 
-	public static function getAll() {
+	public function getAllArticles() {
 		return R::findAll("article", " ORDER BY creation_date DESC");	
 	}
 
-	public function getAllArticleWithTag($tag) {
+	public function getAllArticlesWithTag($tag) {
 		if (!is_string($tag)){
 			throw new InvalidArgumentException($tag ." is not a string");
 		}
-		return R::find("article", "tags LIKE '%" . $tag . "%'");
+		return R::find("article", "tags LIKE ?", array("%". $tag ."%"));
 	}
 
-	public function getAllArticleWithAuthor($author) {
-		if (!is_int($author)){
-			throw new InvalidArgumentException($tag ." is not a string");
+	public function getAllArticlesWithAuthor($author) {
+		if (!is_int($author)) {
+			throw new InvalidArgumentException($author ." is not an int");
 		}
-		return R::find("article", "author == '" . $author . "'");
+		return R::find("article", "author = ?", array($author));
+	}
+	
+	public function getAllArticlesWithSearch($search) {
+		if (!is_string($search)){
+			throw new InvalidArgumentException($search ." is not a string");
+		}
+		$search = "%". $search ."%";
+		return R::find("article", "title LIKE ? OR content LIKE ? OR tags LIKE ?", 
+				array($search, $search, $search));
 	}
 }
 
--- modules/article/template/article.php
+++ modules/article/template/article.php
@@ -9,102 +9,64 @@
 ?>
 
 <?php
-if (isset($_GET["tag"])) {
 
-$posts = Modul::loadModul("article", ROOT)->getAllArticleWithTag($_GET[tag]);
+if (isset($_GET["id"]) && $_GET["id"] > 0) {
 
-foreach ($posts as $article) {
+	$article = Modul::loadModul("article", ROOT)->getArticle((int) $_GET["id"]);
 	$user = R::load("user", $article->author);
-	$article->content = str_replace("\n", "<br>", $article->content);
-	?><article>
-		<h3><a href="?display=article&id=<?php echo $article->id ?>"><?php echo $article->title; ?></a></h3>
+	?><article><?php
+
+	if ($article->id > 0) {
+		$article->content = str_replace("\n", "<br>", $article->content);	
+		?><h3><a href="#"><?php echo $article->title; ?></a></h3>
 		<h6>Written by <a href="?display=article&author=<?php echo $user->id; ?>"><?php echo $user->fullname; ?></a> on <?php echo $article->creation_date; ?>.</h6>
-		<p><?php echo $article->content; ?></p>
-<?php
+		<p><?php echo $article->content; ?></p><?php
+		
 		$tags = explode(";", $article->tags);
 		foreach ($tags as $tag) {
-?>
-			<a href="?display=article&tag=<?php echo $tag; ?>"><?php echo $tag; ?></a>
-<?php
+			?><a href="?display=article&tag=<?php echo $tag; ?>"><?php echo $tag; ?></a><?php
 		}
-	?></article><?php 
-}
-?>
-<?php
-} elseif (isset($_GET["author"]) && $_GET["autor"] > 0) {
-
-$posts = Modul::loadModul("article", ROOT)->getAllArticleWithAuthor((int) $_GET[author]);
-
-foreach ($posts as $article) {
-	$user = R::load("user", $article->author);
-	$article->content = str_replace("\n", "<br>", $article->content);
-	?><article>
-		<h3><a href="?display=article&id=<?php echo $article->id ?>"><?php echo $article->title; ?></a></h3>
-		<h6>Written by <a href="?display=article&author=<?php echo $user->id; ?>"><?php echo $user->fullname; ?></a> on <?php echo $article->creation_date; ?>.</h6>
-		<p><?php echo $article->content; ?></p>
-<?php
-		$tags = explode(";", $article->tags);
-		foreach ($tags as $tag) {
-?>
-			<a href="?display=article&tag=<?php echo $tag; ?>"><?php echo $tag; ?></a>
-<?php
-		}
-	?></article><?php 
-}
-?>
-<?php
- } elseif (isset($_GET["id"]) && $_GET["id"] > 0) {
-
-$article = Modul::loadModul("article", ROOT)->getArticle((int) $_GET["id"]);
-$user = R::load("user", $article->author);
-?>
-
-<article>
-<?php
-if ($article->id > 0) {
-	$article->content = str_replace("\n", "<br>", $article->content);	
-?>
-		<h3><a href="#"><?php echo $article->title; ?></a></h3>
-		<h6>Written by <a href="?display=article&author=<?php echo $user->id; ?>"><?php echo $user->fullname; ?></a> on <?php echo $article->creation_date; ?>.</h6>
-		<p><?php echo $article->content; ?></p>
-<?php
-	$tags = explode(";", $article->tags);
-	foreach ($tags as $tag) {
-?>
-		<a href="?display=article&tag=<?php echo $tag; ?>"><?php echo $tag; ?></a>
-<?php
-	}
-} else {
-?>
-	<h3>No article found</h3>
-<?php
-}
-?>
-</article>
-
-<?php 
-} else {
-
-$posts = Modul::loadModul("article", ROOT)->getAll();
-
-foreach ($posts as $article) {
-	$user = R::load("user", $article->author);
-	$article->content = str_replace("\n", "<br>", $article->content);
-	if(substr_count($article->content, "<br>") > 3){
-		$offset = 0;
-		for( $i = 1; $i <= 3; $i++){
-			$offset = strpos($article->content, "<br>", $offset + 1);
-		}
-		$article->content = substr($article->content, 0, $offset);
 	} else {
-		$article->content = substr($article->content, 0, 400);
+		?><h5>No article was found</h5><?php
 	}
-	?><article>
-		<h3><a href="?display=article&id=<?php echo $article->id ?>"><?php echo $article->title; ?></a></h3>
-		<h6>Written by <a href="?display=article&author=<?php echo $user->id; ?>"><?php echo $user->fullname; ?></a> on <?php echo $article->creation_date; ?>.</h6>
-		<?php echo $article->content; ?>
-		<?php echo '<a href="?display=article&id='. $article->id .'" >More &rarr;</a>'
-	?></article><?php 
+	?></article><?php
+} else {
+	if (isset($_GET["tag"])) {
+		$posts = Modul::loadModul("article", ROOT)->getAllArticlesWithTag($_GET["tag"]);
+		?><h5>All articles with tag: "<?php echo $_GET["tag"]; ?>"</h5><?php 
+	} elseif (isset($_GET["author"]) && $_GET["autor"] > 0) {
+		$posts = Modul::loadModul("article", ROOT)->getAllArticlesWithAuthor((int) $_GET["author"]);
+		?><h5>All articles of author: "<?php echo $_GET["search"]; ?>"</h5><?php
+	} elseif (isset($_GET["search"]) && trim($_GET["search"]) !== "") {
+		$posts = Modul::loadModul("article", ROOT)->getAllArticlesWithSearch($_GET["search"]);
+		?><h5>All search results for: "<?php echo $_GET["search"]; ?>"</h5><?php
+	} else {
+		$posts = Modul::loadModul("article", ROOT)->getAllArticles();
+	}
+
+	if (count($posts) > 0) {
+		foreach ($posts as $article) {
+			$user = R::load("user", $article->author);
+			$article->content = str_replace("\n", "<br>", $article->content);
+			if(substr_count($article->content, "<br>") > 3) {
+				$offset = 0;
+				for($i = 1; $i <= 3; $i++) {
+					$offset = strpos($article->content, "<br>", $offset + 1);
+				}
+				$article->content = substr($article->content, 0, $offset);
+			} else {
+				$article->content = substr($article->content, 0, 400);
+			}
+			?><article>
+				<h3><a href="?display=article&id=<?php echo $article->id ?>"><?php echo $article->title; ?></a></h3>
+				<h6>Written by <a href="?display=article&author=<?php echo $user->id; ?>"><?php echo $user->fullname; ?></a> on <?php echo $article->creation_date; ?>.</h6>
+				<?php echo $article->content; ?>
+				<?php echo '<a href="?display=article&id='. $article->id .'" >More &rarr;</a>'
+			?></article><?php 
+		}
+	} else {
+		?><h6>No articles were found</h6><?php
+	}
 }
-}
+
 ?>
